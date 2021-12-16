1976828f4cd193f7810cd6da1b0fe694752f260f
SethAngell <accounts@sethangell.com>
2021-12-16 10:30:11 -0500

    working on deploy action

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -1,13 +1,9 @@
 # Pull new version
-cd ~/capstone_site;
-pwd;
-git pull;
+cd ~/capstone_site && git pull;
 
 # build and deploy
-cd .ci;
-pwd;
-docker-compose build --no-cache --build-arg loki_user=${loki_user} --build-arg loki_pass=${loki_pass};
-docker-compose up -d;
+cd .ci && docker-compose build --no-cache --build-arg loki_user=${loki_user} --build-arg loki_pass=${loki_pass};
+cd .ci && docker-compose up -d;
 
 # Notify that new version is up
 curl -X POST -d "Body=New version of capstone.sethangell.com has been deployed" -d "From=$twilio_number" -d "To=$to_number" "https://api.twilio.com/2010-04-01/Accounts/$twilio_sid/Messages" -u "$twilio_sid:$twilio_token"
