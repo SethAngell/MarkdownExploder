418dbfba03f27b9a032d6a8119f81d763f721bbb
SethAngell <accounts@sethangell.com>
2021-12-19 22:53:46 -0500

    https cert and dns registration for logging server

--- app/MarkdownExploder.py
+++ app/MarkdownExploder.py
@@ -276,7 +276,7 @@ def generate_logging_handler():
     print(f'{loki_user = }, {loki_pass = }')
     print(f'{"="*120}')
     handler = logging_loki.LokiHandler(
-    url="http://150.136.244.134:3100/loki/api/v1/push", 
+    url="https://loki.logs.doublel.studio/loki/api/v1/push", 
     tags={"application": "MarkdownExploder"},
     auth=(loki_user, loki_pass),
     version="1",
