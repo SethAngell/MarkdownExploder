cf3aef6b51966e8ea32b95cf0627582006958af0
SethAngell <accounts@sethangell.com>
2022-01-03 15:59:20 -0500

    hotfix, needs deployment to server before actions are tested [ci skip]

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -1,8 +1,13 @@
+# Set working directory
+# First attemps to cd into .ci, if that fails try from one level up
+cd .ci || cd capstone_site/.ci
+
 # build and deploy
-cd .ci && docker-compose build --no-cache --build-arg loki_user=${loki_user} --build-arg loki_pass=${loki_pass};
-pwd > 'pwd.log'
-cd .ci && docker-compose up -d;
+docker-compose build --no-cache --build-arg loki_user=${loki_user} --build-arg loki_pass=${loki_pass};
+docker-compose up -d;
 
+if curl 'https://capstone.sethangell.com'
 # Notify that new version is up
-curl -X POST -d "Body=New version of capstone.sethangell.com has been deployed" -d "From=$twilio_number" -d "To=$to_number" "https://api.twilio.com/2010-04-01/Accounts/$twilio_sid/Messages" -u "$twilio_sid:$twilio_token"
-
+then curl -X POST -d "Body=New version of capstone.sethangell.com has been deployed" -d "From=$twilio_number" -d "To=$to_number" "https://api.twilio.com/2010-04-01/Accounts/$twilio_sid/Messages" -u "$twilio_sid:$twilio_token"
+# notify that the curl was not a success
+else curl -X POST -d "Body=The deployment of capstone.sethangell.com seems to have hit a snag" -d "From=$twilio_number" -d "To=$to_number" "https://api.twilio.com/2010-04-01/Accounts/$twilio_sid/Messages" -u "$twilio_sid:$twilio_token"
